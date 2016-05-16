7ca85597c561bebc8583433ddf440be645dd8791
Mihir Gogate <mihir@uber.com>
2016-05-15 22:49:35 -0700

    Water reminder every 1 hr after clicking

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ var path = require('path');
 var BrowserWindow = require('browser-window');
 var util = require('util');
 
-var iconPath = getImagePath('green.png');
 var appIcon = null;
 var win = null;
 
@@ -13,33 +12,69 @@ function getImagePath(imageFileName) {
     return path.join(__dirname, 'images/' + imageFileName);
 }
 
+function shouldToggle(timestampOfLastDrink, currentTimestamp, secondsToWait) {
+    if (timestampOfLastDrink === null) {
+        return true;
+    }
+    if ((currentTimestamp - timestampOfLastDrink) > secondsToWait) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function Beeper(beeperStopCallback) {
+    var STEADY_STATE = 0;
+    var state = STEADY_STATE;
+    var iconPath = getImagePath('green.png');
+    var appIcon = new Tray(iconPath);
+    var contextMenu = Menu.buildFromTemplate([
+        {
+        label: 'Drank!',
+        type: 'normal',
+        icon: getImagePath('water.png'),
+        click: beeperStopCallback,
+        },
+        { label: 'Quit',
+        accelerator: 'Command+Q',
+        selector: 'terminate:',
+        }
+    ]);
+    appIcon.setToolTip('Drink Water');
+    appIcon.setContextMenu(contextMenu);
+    return {
+        toggle: function() {
+            if (state == 0) {
+                state = 1;
+                appIcon.setImage(getImagePath('red.png'));
+            } else {
+                state = 0;
+                appIcon.setImage(getImagePath('green.png'));
+            }
+        },
+        steady: function() {
+            if (state == 1) {
+                state = 0;
+                appIcon.setImage(getImagePath('green.png'));
+            }
+        }
+    }
+}
+
+function getEpoch() {
+    return (new Date()).getTime() / 1000;
+}
+
 app.on('ready', function(){
-  var action = 0;
-  var appIcon = new Tray(iconPath);
+  var timestampOfLastDrink = null;
+  var beeper = Beeper(function() {
+    timestampOfLastDrink = getEpoch();
+  });
   setInterval(function() {
-    if (action == 0) {
-        appIcon.setImage(getImagePath('red.png'));
-        action = 1;
-    } else if (action == 1) {
-        appIcon.setImage(getImagePath('green.png'));
-        action = 0;
+    if (shouldToggle(timestampOfLastDrink, getEpoch(), 3600)) {
+        beeper.toggle();
+    } else {
+        beeper.steady();
     }
   }, 200);
-  var contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Drank!',
-      type: 'normal',
-      icon: getImagePath('water.png'),
-      click: function(e) {
-        action = 3;
-        console.log(e);
-      }
-    },
-    { label: 'Quit',
-      accelerator: 'Command+Q',
-      selector: 'terminate:',
-    }
-  ]);
-  appIcon.setToolTip('Drink Water');
-  appIcon.setContextMenu(contextMenu);
 });
